a7815b1a641e00c0074db481a7c1632e3d163bbf
sonthaya2799 <81342630+sonthaya2799@users.noreply.github.com>
2021-07-08 16:06:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'uF9wRMEgSLumUhtawk4cAJ5tgCYoDkuwirAxjDzf1DMptT7gbHS6z0+GEB4vICrFlCnwKkY9FzH7+06cWTVfAbe+FbOox310CM2bOTf59tHOz1ZTSob3tyd/XEDolRd9W/HWyCZwARevRq06bClUxQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
